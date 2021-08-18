6ca939c9e89b671f3fe91b3df0922c588ed7ce28
UnderJonh <70285135+UnderJonh@users.noreply.github.com>
2021-08-18 17:32:43 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+def soma (a,b):
+    return a+b
+
+def subtracao (a,b):
+    return a-b
+
+def multiplicacao (a,b):
+    return a*b
+
+def divisao (a,b):
+    return a/b
+
+def potenciacao (a,b):
+    return maht.pow(a,b)
+
---
+++ poi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+i = int(input('Digite um inteiro: '))
+
+if i%2 :
+    print("💥 Seu Numero Seu Numero é Ímpar💥")
+else:
+    print("💥Seu Numero é Par💥")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
